c444e5a5c94df7bc9e3a7295734694c69a14003a
Douglas Junior <douglasmelere@gmail.com>
2024-12-12 19:16:48 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { env } from "./env";
 import { app } from "./app";
 
 app.listen({
+  host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
   port: env.PORT
 }).then(() => {
   console.log('HTTP Server Running!')
