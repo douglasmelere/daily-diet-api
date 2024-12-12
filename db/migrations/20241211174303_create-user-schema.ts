75b9e035ac15cd565753ae285ba345836e0ab1d6
Douglas Junior <douglasmelere@gmail.com>
2024-12-12 20:01:18 -0300

    Update 20241211174303_create-user-schema.ts

--- db/migrations/20241211174303_create-user-schema.ts
+++ db/migrations/20241211174303_create-user-schema.ts
@@ -7,11 +7,10 @@ export async function up(knex: Knex): Promise<void> {
     table.text('name').notNullable()
     table.text('email').notNullable()
     table.text('address').notNullable()
-    table.integer('weight').notNullable()
+    table.decimal('weight', 5, 2).notNullable()
     table.decimal('height', 5, 2).notNullable()
   })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTable('users')
